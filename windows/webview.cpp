6459804d83059acdd0aefec63c441fe352869452
cdosoftei <ciprian.dosoftei@gmail.com>
2024-06-07 17:59:43 -0400

    windows stub

--- windows/webview.cpp
+++ windows/webview.cpp
@@ -18,7 +18,6 @@ struct uiWebView {
 	HWND hwnd;
 	ICoreWebView2Environment *env;
 	ICoreWebView2EnvironmentOptions *options;
-	ICoreWebView2Controller *controller;
 	ICoreWebView2 *webView;
 };
 
@@ -73,23 +72,33 @@ void uiWebViewRequestRespond(void *request, const char *body, size_t length, con
 
 void uiWebViewSetHtml(uiWebView *w, const char *html)
 {
+	/*
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 	std::wstring wideHtml = converter.from_bytes(html);
 	w->webView->NavigateToString(wideHtml.c_str());
+	*/
 }
 
 void uiWebViewSetUri(uiWebView *w, const char *uri)
 {
+	/*
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 	std::wstring wideUri = converter.from_bytes(uri);
 	w->webView->Navigate(wideUri.c_str());
+	*/
 }
 
 void uiWebViewEval(uiWebView *w, const char *js)
 {
+	/*
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 	std::wstring wideJs = converter.from_bytes(js);
 	w->webView->ExecuteScript(wideJs.c_str(), nullptr);
+	*/
+}
+
+static void onResize(uiWindowsControl *c)
+{
 }
 
 uiWebView *uiNewWebView(void)
@@ -101,24 +110,66 @@ uiWebView *uiNewWebView(void)
 
 	uiWindowsNewControl(uiWebView, w);
 
-	hr = CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr, Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
-		[w](HRESULT result, ICoreWebView2Environment *env) -> HRESULT {
-			w->env = env;
-			printf("CreateCoreWebView2EnvironmentWithOptions\n");
-			return env->CreateCoreWebView2Controller(w->hwnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
-				[w](HRESULT result, ICoreWebView2Controller *controller) -> HRESULT {
-					printf("CreateCoreWebView2Controller\n");
-					w->controller = controller;
-					controller->get_CoreWebView2(&w->webView);
-					return S_OK;
-				}).Get());
+	w->hwnd = uiWindowsEnsureCreateControlHWND(0,
+		L"static", L"",
+		SS_LEFTNOWORDWRAP | SS_NOPREFIX,
+		hInstance, NULL,
+		TRUE);
+
+	hr = CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+		Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+			[w](HRESULT result, ICoreWebView2Environment *env) -> HRESULT {
+				if (result != S_OK) {
+					logHRESULT(L"error creating WebView2 environment", result);
+					return result;
+				}
+
+				w->env = env;
+
+				HRESULT hr = env->CreateCoreWebView2Controller(w->hwnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+					[w](HRESULT result, ICoreWebView2Controller *controller) -> HRESULT {
+						if (result != S_OK) {
+							logHRESULT(L"error creating WebView2 controller", result);
+							return result;
+						}
+
+						HRESULT hr = controller->get_CoreWebView2(&w->webView);
+						if (hr != S_OK) {
+							logHRESULT(L"error getting WebView2 interface", hr);
+							return hr;
+						}
+
+						printf("webView created: %p\n", w->webView);
+
+						hr = w->webView->Navigate(L"https://www.google.com/");
+						if (hr != S_OK) {
+							logHRESULT(L"error navigating to URL", hr);
+							return hr;
+						} else {
+							printf("navigated to URL\n");
+						}
+/*
+						RECT rect;
+						GetWindowRect(w->hwnd, &rect);
+						char *posStr = (char *)malloc(100);
+						sprintf(posStr, "hwnd: %p, %d, %d, %d, %d\n", w->hwnd, rect.left, rect.top, rect.right, rect.bottom);
+
+						HDC hdc = GetDC(w->hwnd);
+						GetClientRect(w->hwnd, &rect);
+						DrawTextA(hdc, posStr, strlen(posStr), &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+						ReleaseDC(w->hwnd, hdc);
+*/
+						return S_OK;
+					}).Get());
+
+				if (hr != S_OK) {
+					logHRESULT(L"error creating WebView2 controller", hr);
+					return hr;
+				}
+
+				return S_OK;
 		}).Get());
-
-	if (hr != S_OK) {
-		logHRESULT(L"error creating webview", hr);
-		return NULL;
-	}
-
+	
 	printf("uiNewWebView done\n");
 
 	return w;
